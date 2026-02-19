6654b72d04d2dd43aa0d96ef2114894f9a567993
Biswaranjan <biswaranjan.mohapatra@swallowstack.com>
2026-02-19 22:45:56 +0530

    initial Commit

---
+++ src/main/java/com/food/FoodDeliveryBackendApplication.java
@@ -0,0 +1,15 @@
+package com.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryBackendApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(FoodDeliveryBackendApplication.class, args);
+        System.out.println("Application Started..");
+	}
+
+}
